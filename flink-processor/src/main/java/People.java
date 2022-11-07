a39d9d5a31babfe53d02a595e21acba9bb5f7ca8
sandeep-rafay <sandeep.kongathi@rafay.co>
2022-11-07 20:48:46 +0530

    updated

---
+++ flink-processor/src/main/java/People.java
@@ -0,0 +1,49 @@
+
+
+
+public class People {
+
+    public String id;
+    public String name;
+    public String brand;
+
+    public String date;
+
+    public People() {
+    }
+
+    public People(String id, String name, String brand, String date) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.date = date;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        final java.lang.StringBuilder sb = new java.lang.StringBuilder("People{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", brand='").append(brand).append('\'');
+        sb.append(", date='").append(date).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        People people = (People) o;
+        return id.equals(people.id) && name == people.name && brand == people.name && date == people.date;
+
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, name, brand, date);
+    }
+}
